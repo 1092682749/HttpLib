05eaa2fddb6cd1d8e46a420328beb64080bfd146
1092682749 <dyz1092682749@gmail.com>
2020-03-03 19:52:51 +0800

    =init

---
+++ net/src/main/java/com/dyz/NetWorkServerListener.java
@@ -0,0 +1,8 @@
+package com.example.qingyun.myfirstapp.utils;
+
+import java.util.Hashtable;
+
+public interface NetWorkServerListener {
+    public void onSuccessed(Object response);
+    public void onFailed(Object message);
+}
